aca71a44a78a48443e4025abb26ff967491cfb24
Agung Satya <i.g.b.n.satyawibawa@gmail.com>
2022-07-22 09:32:32 +0800

    scroll to top every navigate

--- src/App.js
+++ src/App.js
@@ -9,55 +9,58 @@ import ItemMainPage from "./containers/ItemMainPage";
 import MovieTVDetailPage from "./containers/MovieTVDetailPage";
 import PeopleDetailPage from "./containers/PeopleDetailPage";
 import DashboardPage from "./containers/Dashboard";
+import ScrollToTop from "./templates/ScrollToTop";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route
-          path="/login"
-          element={
-            <UnProtectedAuth>
-              <LoginPage />
-            </UnProtectedAuth>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <UnProtectedAuth>
-              <RegisterPage />
-            </UnProtectedAuth>
-          }
-        />
-        <Route
-          path="/profiles"
-          element={
-            <ProtectedAuth>
-              <ProfilesPage />
-            </ProtectedAuth>
-          }
-        />
-        <Route path="/src/:type" element={<ItemMainPage />} />
-        <Route
-          path="/src/persons/detail/:id"
-          element={
-            <ProtectedAuth>
-              <PeopleDetailPage />
-            </ProtectedAuth>
-          }
-        />
-        <Route
-          path="/src/:type/detail/:id"
-          element={
-            <ProtectedAuth>
-              <MovieTVDetailPage />
-            </ProtectedAuth>
-          }
-        />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route
+            path="/login"
+            element={
+              <UnProtectedAuth>
+                <LoginPage />
+              </UnProtectedAuth>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <UnProtectedAuth>
+                <RegisterPage />
+              </UnProtectedAuth>
+            }
+          />
+          <Route
+            path="/profiles"
+            element={
+              <ProtectedAuth>
+                <ProfilesPage />
+              </ProtectedAuth>
+            }
+          />
+          <Route path="/src/:type" element={<ItemMainPage />} />
+          <Route
+            path="/src/persons/detail/:id"
+            element={
+              <ProtectedAuth>
+                <PeopleDetailPage />
+              </ProtectedAuth>
+            }
+          />
+          <Route
+            path="/src/:type/detail/:id"
+            element={
+              <ProtectedAuth>
+                <MovieTVDetailPage />
+              </ProtectedAuth>
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </ScrollToTop>
     </>
   );
 }
